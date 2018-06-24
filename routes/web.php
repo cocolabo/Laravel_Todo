16eb1dc3da1f24d13d6af3449ee80ed563f1b9ce
Hiroki Kato <cocomaru@HirokinoMacBook-puro.local>
2018-06-24 22:07:16 +0900

    add toao app

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/**
+ * タスクダッシュボード表示
+ */
+Route::get('/','TaskController@index');
+
+/**
+ * 新タスク追加
+ */
+Route::post('/task','TaskController@store');
+
+/**
+ * タスク削除
+ */
+Route::delete('/task/{task}','TaskController@destroy');
